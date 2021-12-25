253230108bf372815a25425b412eccb52b408599
Yuheng Ding <d.yuheng@lotus.engr.wustl.edu>
2021-12-25 12:43:37 -0600

    created sample topology for branch and bound knapsack

---
+++ SubProblem.cuh
@@ -0,0 +1,11 @@
+#ifndef __SUB_PROBLEM
+#define __SUB_PROBLEM
+
+struct SubProblem {
+  unsigned int currentItem;
+  double upperBound;
+  double currentTotalProfit;
+  double currentTotalWeight;
+}
+
+#endif
